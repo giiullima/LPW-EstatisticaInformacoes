d57680676cec5bf55a9c6d50046b2d33ccee7733
Giulliany Lima Bezerra <53629399+Giulliany@users.noreply.github.com>
2020-03-26 12:27:33 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EstatisticasInformacoes';
+  nome = null;
+  idade = null;
+  pessoas = [];
+  totalizador = null;
+  jovem =  null;
+  velho = null;
+
+  adicionar() {
+    this.pessoas.push({nome:this.nome, idade:this.idade});
+    this.nome = null;
+    this.idade = null;
+    this.totalizador +=1;
+    this.selecao();
+  }
+  
+  excluir(id) {
+    this.pessoas.splice(this.pessoas.indexOf(id),1);
+    this.totalizador -=1
+    this.selecao();
+  }
+
+  selecao(){
+    var idades = this.pessoas.map( ({idade}) => idade );
+
+    var max = Math.max(...idades);
+    var min = Math.min(...idades);
+
+    this.jovem = this.pessoas.find(({ idade }) => idade === min);
+    this.velho = this.pessoas.find(({ idade }) => idade === max);
+    
+  }
 }
