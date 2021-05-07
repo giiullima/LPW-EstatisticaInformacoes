7fcffb18d02794f456299b04c6d89196f5bfc56f
Leonardo Setubal <setubal43@gmail.com>
2021-05-06 21:13:27 -0300

    fix adicionado subscriber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { ListObservable, UserSubject, EstaticObservable } from './_subscribeManager';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  nome = null;
-  idade = null;
-  pessoas = [];
-  totalizador = null;
-  jovem =  null;
-  velho = null;
-
-  adicionar() {
-    this.pessoas.push({nome:this.nome, idade:this.idade});
-    this.nome = null;
-    this.idade = null;
-    this.totalizador +=1;
-    this.selecao();
-  }
-  
-  excluir(id) {
-    this.pessoas.splice(this.pessoas.indexOf(id),1);
-    this.totalizador -=1
-    this.selecao();
+export class AppComponent implements OnInit {
+  public formUser : FormGroup
+  private _userSubject: UserSubject
+  private _observerList: ListObservable
+  private _observerEstatic: EstaticObservable
+  constructor(
+    private fb:FormBuilder,
+  ){
+    this._userSubject =  new UserSubject()
+    this._observerList = new ListObservable()
+    this._observerEstatic = new EstaticObservable()
+    this._userSubject.addObservable(this._observerList)
+    this._userSubject.addObservable(this._observerEstatic)
   }
 
-  selecao(){
-    var idades = this.pessoas.map( ({idade}) => idade );
-
-    var max = Math.max(...idades);
-    var min = Math.min(...idades);
-
-    this.jovem = this.pessoas.find(({ idade }) => idade === min);
-    this.velho = this.pessoas.find(({ idade }) => idade === max);
-    
+  ngOnInit(): void {
+    this.initForm()
   }
+
+  initForm(){
+    this.formUser = this.fb.group({
+      nome:new FormControl(''),
+      email: new FormControl(''),
+      birthday: new FormControl(Date)
+    })
+  }
+
+  adicionar(){
+    this._userSubject.addUser(this.formUser.value)
+  }
+
+  remover(index:number){
+    this._userSubject.removeUser(index)
+  }
+
+  get listPessoas(){
+    return this._observerList.getList
+  }
+
+  get getStatistic(){
+    return this._observerEstatic.getObj
+  }
+
 }
+
